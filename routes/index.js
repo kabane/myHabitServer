6f4696365ff261d8cdc87de2804b998983d0678d
kabane <kabaneshi@gmail.com>
2018-09-23 23:19:04 +0900

    todoモデル作成 & 参照系の処理実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myHabit', {useNewUrlParser: true})
+var Todo = require( '../models/todo')
+
+router.get( ['/', '/todo'], function ( req, res ) {
+  Todo.find({}, function(err, todo) {
+    if (err) {
+      throw err;
+    } else {
+      res.send(todo);
+    }
+  });
 });
+
+router.get( '/todo/:id', function ( req, res ) {
+  Todo.findOne({_id: req.params.id}, function(err, todo){
+    if (err) {
+      res.send(err)
+    }
+    res.json(todo)
+  });
+});
+
+router.post( '/todo', function ( req, res ) {
+  var data = req.body;
+  var todo = new model.Todo({
+    _id: req.body._id,
+    name: req.body.name
+  })
+
+  todo.save(function(err){
+    if (err) {
+      res.send(err)
+    }
+    res.json({message: "create " + todo.text})
+  })
+} );
 
 module.exports = router;
