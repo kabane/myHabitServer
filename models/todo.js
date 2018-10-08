d01d447c82a8daed9af1222860cb81d860897f60
kabane <kabaneshi@gmail.com>
2018-10-09 02:04:44 +0900

    Todoテーブルのプロパティ名変更

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema({
-    text: String
+    name: String,
+    status: Number
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
