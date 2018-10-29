2dd3122c0a0eb53339712a5c8bd20beaaee283c5
kabane <kabaneshi@gmail.com>
2018-10-29 10:16:14 +0900

    初期化用のスクリプト生成

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -1,48 +1,48 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema
+    Schema = mongoose.Schema,
+    Todo = require('../models/todo'),
+    Category = require('../models/category');
 
 mongoose.connect('mongodb://localhost:27017/myHabit', {useNewUrlParser: true})
 
-todos = [
-    {name: 'todo1', status: 0},
-    {name: 'todo2', status: 0},
-    {name: 'todo3', status: 0},
-    {name: 'todo4', status: 0},
-    {name: 'todo5', status: 1},
-    {name: 'todo6', status: 1},
-    {name: 'todo7', status: 1},
-    {name: 'todo8', status: 1}
-]
-
-var TodoSchema = new Schema({
-    name: String,
-    status: Number
-})
-var Todo = mongoose.model('Todo', TodoSchema)
-
-Todo.deleteMany({}, function(err){
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Deleted Todo')
+Category.deleteMany({})
+.then(
+    function () {
+        console.log('create Category');
+        return Category.insertMany([
+            {name: '開発'},
+            {name: '雑務'},
+            {name: 'その他'}
+        ]);
     }
-})
+)
+.then(
+    function() {
+        console.log('delete Todo')
+        return Todo.deleteMany({})
+    }
+)
+.then(
+    function() {
+        return Category.find({}, function(e, category){
+            console.log('create Todo')
+            return Todo.insertMany([
+                {name: 'todo1', status: 0, categories: category[0]},
+                {name: 'todo2', status: 0, categories: category[1]},
+                {name: 'todo3', status: 0, categories: category[2]}
+            ])
+        })
+    }
+)
+.then(
+    function () {
+        process.exit(1);
+    }
+)
+.catch(
+    function() {
+        console.error(err)
+        process.exit(1)
+    }
+)
 
-for(var i = 0; i < todos.length; i++) {
-    var todo = new Todo({
-        name: todos[i].name,
-        status: todos[i].status
-    })
-
-    todo.save(function(err){
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("Success Todo Created")
-        }
-    })
-}
-
-process.on("exit", function() {
-    process.exit(1);
-});
