5046b3f6d0dd1341310b33bcf4220f5b78679c54
kabane <kabaneshi@gmail.com>
2018-11-26 03:26:59 +0900

    seedファイル微修正

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose'),
     Todo = require('../models/todo'),
     Category = require('../models/category');
 
-mongoose.connect('mongodb://localhost:27017/myHabit', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/myHabit', {useNewUrlParser: true});
 
 Category.deleteMany({})
 .then(
@@ -18,19 +18,19 @@ Category.deleteMany({})
 )
 .then(
     function() {
-        console.log('delete Todo')
-        return Todo.deleteMany({})
+        console.log('delete Todo');
+        return Todo.deleteMany({});
     }
 )
 .then(
     function() {
         return Category.find({}, function(e, category){
-            console.log('create Todo')
+            console.log('create Todo');
             return Todo.insertMany([
                 {name: 'todo1', status: 0, elapsed_time: 0, category_id: category[0]},
                 {name: 'todo2', status: 0, elapsed_time: 0, category_id: category[1]},
                 {name: 'todo3', status: 0, elapsed_time: 0, category_id: category[2]}
-            ])
+            ]);
         })
     }
 )
@@ -41,8 +41,8 @@ Category.deleteMany({})
 )
 .catch(
     function() {
-        console.error(err)
-        process.exit(1)
+        console.error(err);
+        process.exit(1);
     }
 )
 
